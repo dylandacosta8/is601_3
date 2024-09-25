b29ef5f7568985540c711e9ecfdfc44ce26d054c
Dylan Da Costa <dylan2222@gmail.com>
2024-09-25 14:09:26 -0400

    updated tests to align with oop setup, added calculator hisory tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,24 +1,53 @@
-'''My Calculator Tests'''
+''' Tests '''
 import pytest
-from calculator import add, subtract, multiply, divide
+from calculator import Calculator
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(6,2) == 8
+class TestCalculator:
+    ''' CLass for Testing the Calculator Functions '''
 
-def test_subtraction():
-    '''Test that subtraction function works '''    
-    assert subtract(5,2) == 3
+    def setup_method(self):
+        ''' Resets history before each test '''
+        Calculator.clear_history()
 
-def test_multiplication():
-    '''Test that multiplication function works '''    
-    assert multiply(8,8) == 64
+    def test_add(self):
+        ''' Tests for addition '''
+        calc = Calculator()
+        assert calc.add(1, 2) == 3
 
-def test_division():
-    '''Test that division function works '''    
-    assert divide(5,5) == 1.0
+    def test_subtract(self):
+        ''' Tests for subtraction '''
+        calc = Calculator()
+        assert calc.subtract(5, 3) == 2
 
-def test_division_by_zero():
-    '''Test that divide by zero should not work '''    
-    with pytest.raises(ValueError,match="Division by zero is not allowed"):
-        assert divide(10,0)
+    def test_multiply(self):
+        ''' Tests for multiplication '''
+        calc = Calculator()
+        assert calc.multiply(2, 3) == 6
+
+    def test_divide(self):
+        ''' Tests for regular division '''
+        calc = Calculator()
+        assert calc.divide(6, 3) == 2
+
+    def test_divide_by_zero(self):
+        ''' Tests to catch divide by zero exception '''
+        calc = Calculator()
+        with pytest.raises(ZeroDivisionError):
+            calc.divide(6, 0)
+
+    def test_history(self):
+        ''' Tests to check if history is accessible '''
+        calc = Calculator()
+        calc.add(1, 1)
+        calc.subtract(2, 1)
+        history = calc.get_history()
+        assert len(history) == 2
+        assert "Add" in history[0]
+        assert "Subtract" in history[1]
+
+    def test_clear_history(self):
+        ''' Tests to check if clearing history works '''
+        calc = Calculator()
+        calc.add(1, 1)
+        calc.clear_history()
+        assert len(calc.get_history()) == 0
